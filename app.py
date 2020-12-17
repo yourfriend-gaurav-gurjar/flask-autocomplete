01538d717ddbdf62893ab44b8d54de97e43791c1
alexnesov <alexandrenesovic@gmail.com>
2020-12-17 19:20:14 +0100

    first

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, Response, render_template, request
+import json
+from wtforms import TextField, Form
+
+app = Flask(__name__)
+
+class SearchForm(Form):
+    autocomp = TextField('Insert City', id='city_autocomplete')
+
+
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    cities = ["Olongapo City",
+          "Angeles City",
+          "Manila",
+          "Makati",
+          "Pasig",
+          "Davao",
+          "Cebu",
+          "Quezon City",
+          "Taguig"]
+    print(cities)    
+    return Response(json.dumps(cities), mimetype='application/json')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchForm(request.form)
+    return render_template("search.html", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
